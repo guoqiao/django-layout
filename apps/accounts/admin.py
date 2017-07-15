cff6a6b3b0cd10dd579ba67e38bc7eb55811c88f
Joe Guo <joeg@catalyst.net.nz>
2017-07-15 22:53:09 +1200

    unregister User from admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from . import models as m
+
+admin.site.unregister(User)
 
 
 class PreferenceInline(admin.TabularInline):
